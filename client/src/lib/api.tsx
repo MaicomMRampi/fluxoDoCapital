0580503be485902dbe30343512d6eb325aad1a98
Maicom <maicom.azus@gmail.com>
2024-11-03 13:46:12 -0300

    novo teste

--- client/src/lib/api.tsx
+++ client/src/lib/api.tsx
@@ -1,11 +1,9 @@
 import axios from "axios";
 export const api = axios.create({
 
-    // baseURL: "http://localhost:3333/api"
+
     baseURL: "https://app.fluxodocapital.com.br/api"
-    // baseURL: process.env.NODE_ENV === 'development'
-    //     ? "https://hp.gedagro.com.br/api"
-    //     : "https://hp.gedagro.com.br/api"
+
 })
 
 // "https://hp.gedagro.com.br/api"
