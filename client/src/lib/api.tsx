993ca1685baf30ca904ff4f9bd5304ad3f1076a1
Maicom <maicom.azus@gmail.com>
2024-11-04 10:29:58 -0300

    ajuuj

--- client/src/lib/api.tsx
+++ client/src/lib/api.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 export const api = axios.create({
-    baseURL: "http://localhost:3333/api"
+    // baseURL: "http://localhost:3333/api"
 
-    // baseURL: "https://app.fluxodocapital.com.br/api"
+    baseURL: "https://app.fluxodocapital.com.br/api"
 
 })
 
